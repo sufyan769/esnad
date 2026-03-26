f57aa0fe9d91b3e4e516faa37549807cdfe7267f
x max <xmax@192.168.1.109>
2026-03-26 23:10:31 +0300

    Migrate all static HTML files to public directory and add rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,46 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/index.html'
+      },
+      {
+        source: '/fatwa',
+        destination: '/fatwa.html'
+      },
+      {
+        source: '/hadith/:id',
+        destination: '/details_hadith.html?id=:id'
+      },
+      {
+        source: '/scholar/:id',
+        destination: '/details_scholars.html?id=:id'
+      },
+      {
+        source: '/history',
+        destination: '/history.html'
+      },
+      {
+        source: '/history/:id',
+        destination: '/history.html?id=:id'
+      },
+      {
+        source: '/jarh/:id',
+        destination: '/details_jarh.html?id=:id'
+      }
+    ];
+  },
+  async redirects() {
+    return [
+      {
+        source: '/fatwa_pages/fatwa_:id.html',
+        destination: '/fatwa/:id',
+        permanent: true
+      }
+    ];
+  }
 };
 
 export default nextConfig;
